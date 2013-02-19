683d462dcf3a4e3633b8a08a4fd425af0e8273ed
Michael Wisely <michaelwisely@gmail.com>
2013-02-18 22:05:34 -0600

    Added accounts urls to simple_urls to make simple-django happy

--- project/simple_urls.py
+++ project/simple_urls.py
@@ -8,8 +8,13 @@ urlpatterns = patterns(
     '',
 
     url(r'', include(competition.urls)),
-    url (r'^accounts/login/$', 'django.contrib.auth.views.login',
-         {'template_name': 'accounts/login.html'}),
+    url (r'^accounts/login/$',
+         'django.contrib.auth.views.login',
+         {'template_name': 'accounts/login.html'},
+         name='account_login'),
+    url (r'^accounts/logout/$',
+         'django.contrib.auth.views.logout_then_login',
+         name='account_logout'),
     )
 
 if settings.DEBUG:
