0c688d9242e4eca602b05e13311b4b622e5a3778
Michael Wisely <michaelwisely@gmail.com>
2013-02-20 22:08:51 -0600

    Use crispy forms for team create

--- src/competition/forms/team_forms.py
+++ src/competition/forms/team_forms.py
@@ -1,5 +1,9 @@
 from django import forms
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit
+from crispy_forms.bootstrap import FormActions
+
 from competition.models.team_model import Team
 
 
@@ -7,6 +11,21 @@ class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
         fields = ('name', )
+
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.layout = Layout(
+            Fieldset(
+                'Create a new team',
+                'name',
+            ),
+            FormActions(
+                Submit('submit', 'Submit')
+            )
+        )
+        super(TeamForm, self).__init__(*args, **kwargs)
+
 
     def validate_unique(self):
         exclude = self._get_validation_exclusions()
