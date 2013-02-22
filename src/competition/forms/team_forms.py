a99995b12d782b2cab25f570e8d77a3fd9a0a00b
Michael Wisely <michaelwisely@gmail.com>
2013-02-22 17:25:53 -0600

    Properly catch duplicate team names for a competition

--- src/competition/forms/team_forms.py
+++ src/competition/forms/team_forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.template.defaultfilters import slugify
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, Submit
@@ -26,6 +27,12 @@ class TeamForm(forms.ModelForm):
         )
         super(TeamForm, self).__init__(*args, **kwargs)
 
+    def clean_name(self):
+        c = self.instance.competition
+        s = slugify(self.cleaned_data['name'])
+        if Team.objects.filter(competition=c, slug=s).exists():
+            msg = "This name is already taken for %s" % c.name
+            raise forms.ValidationError(msg)
 
     def validate_unique(self):
         exclude = self._get_validation_exclusions()
