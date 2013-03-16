d04b8adb027735f099e051fa9104082a41118e54
Michael Wisely <michaelwisely@gmail.com>
2013-03-16 17:30:51 -0500

    Add team1/2 fields Game
    This makes for much faster queries

--- src/competition/models/game_model.py
+++ src/competition/models/game_model.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 from competition.models.competition_model import Competition
+from competition.models.team_model import Team
 
 import json
 
@@ -24,11 +25,18 @@ class Game(models.Model):
 
     competition = models.ForeignKey(Competition)
 
-    # Default to JSON null, which gets loaded as None
-    extra_data = models.TextField(null=True, default="null")
-
+    created = models.DateTimeField(auto_now_add=True, null=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+
+    team1 = models.ForeignKey(Team, related_name="+", null=True)
+    team1_score = models.IntegerField(null=True)
+
+    team2 = models.ForeignKey(Team, related_name="+", null=True)
+    team2_score = models.IntegerField(null=True)
+
+    # Default to JSON null, which gets loaded as None
+    extra_data = models.TextField(null=True, default="null")
 
     def __unicode__(self):
         return "Game #%d" % self.id
