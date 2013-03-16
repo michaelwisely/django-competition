1a14a573397df3ea7d09fa13c0ede6b915b5b0f2
Michael Wisely <michaelwisely@gmail.com>
2013-03-16 13:28:49 -0500

    Add an "extra_data" field to Game model

--- src/competition/models/game_model.py
+++ src/competition/models/game_model.py
@@ -3,6 +3,8 @@ from django.core.exceptions import ValidationError
 
 from competition.models.competition_model import Competition
 
+import json
+
 
 class Game(models.Model):
     class Meta:
@@ -10,9 +12,16 @@ class Game(models.Model):
 
     competition = models.ForeignKey(Competition)
 
+    extra_data = models.TextField(null=True)
+
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
     def clean(self):
         if self.start_time > self.end_time:
             raise ValidationError("Start time is after end time.")
+        try:
+            if self.extra_data:
+                json.loads(self.extra_data)
+        except ValueError:
+            raise ValidationError("Invalid JSON string.")
