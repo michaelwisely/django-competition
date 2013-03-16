c666c46d71567276acc6d4979bf14187d1ac2277
Michael Wisely <michaelwisely@gmail.com>
2013-03-16 15:36:54 -0500

    Add docs for Game
    Explains how the "display" thing works for extra_data

--- src/competition/models/game_model.py
+++ src/competition/models/game_model.py
@@ -7,6 +7,18 @@ import json
 
 
 class Game(models.Model):
+    """Game Model
+
+    To store additional game data, serialize it as a JSON string and
+    store it in the extra_data field. If you want the data to be
+    available to the user, include it under the "display" key like so:
+
+    {"display": {"location": "place"},
+     "referee": "tim"}
+
+    If this information was the extra_data for an object, "location"
+    would be displayed for the user, but "referee" would remain hidden.
+    """
     class Meta:
         app_label = 'competition'
 
