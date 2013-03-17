0914511edeef8a25d39b4f13fd8c6cb3023b7f56
Michael Wisely <michaelwisely@gmail.com>
2013-03-16 19:05:49 -0500

    Check for AnonymousUser when getting games

--- src/competition/views/game_views.py
+++ src/competition/views/game_views.py
@@ -2,12 +2,20 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
+from django.http import Http404
 
 from ..models import Game
 from .mixins import CompetitionViewMixin, RequireRunningMixin
 
 
 class GameView(CompetitionViewMixin, RequireRunningMixin):
+
+    def get_team(self):
+        if self.request.user.is_anonymous():
+            raise Http404("Anonymous user cannot view games")
+        return get_object_or_404(self.request.user.team_set,
+                                 competition=self.get_competition())
+
     def parse_data(self, games):
         fields = set()
         for game in games:
@@ -24,8 +32,7 @@ class GameListView(GameView, ListView):
     template_name = 'competition/game/game_list.html'
 
     def get_queryset(self):
-        self.team = get_object_or_404(self.request.user.team_set,
-                                      competition=self.get_competition())
+        self.team = self.get_team()
         q = Game.objects.prefetch_related('team1', 'team2', 'competition')
         q = q.filter(Q(team1=self.team)|Q(team2=self.team))
         return q.order_by('start_time')
@@ -41,8 +48,7 @@ class GameDetailView(GameView, DetailView):
     template_name = 'competition/game/game_detail.html'
 
     def get_queryset(self):
-        self.team = get_object_or_404(self.request.user.team_set,
-                                      competition=self.get_competition())
+        self.team = self.get_team()
         return Game.objects.filter(Q(team1=self.team)|Q(team2=self.team))
 
     def get_context_data(self, **kwargs):
