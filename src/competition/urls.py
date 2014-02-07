b557f172e7ef2ccfebc99410fc097a5baca6e6fa
Michael Wisely <michaelwisely@gmail.com>
2014-02-06 22:48:28 -0600

    Fix imports of url functions

--- src/competition/urls.py
+++ src/competition/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls import patterns, url, include
 
 from competition.views.competition_views import CompetitionListView
 from competition.views.competition_views import CompetitionDetailView
