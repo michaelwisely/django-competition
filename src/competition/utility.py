745e034a4cb071737c2e166afac40b109af9b813
Michael Wisely <michaelwisely@gmail.com>
2014-04-10 23:20:45 -0500

    Remove old typeahead API stuff
    The ol' bootstrap 2 stuff

--- src/competition/utility.py
+++ src/competition/utility.py
@@ -1,24 +1,4 @@
-from django.db.models import Q
 from PIL import Image
-
-
-def competitor_search_filter(queryset, search):
-    # Split the full name on spaces
-    if search != None:
-        search = search.split(" ")
-    else:
-        search = []
-
-    # Create queries to check username, first_name, and last_name
-    query = [Q(username__icontains=part) |
-             Q(first_name__icontains=part) |
-             Q(last_name__icontains=part) for part in search]
-
-    # Combine the queries into one monster query
-    query = reduce(lambda x, y: x | y, query)
-
-    # Execute the query and return only unique results
-    return queryset.filter(query).distinct()
 
 
 def create_thumbnail(original_path, thumb_path, thumb_size):
